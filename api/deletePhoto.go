e91f9abaada56af422991057084362da82653dba
Snikimonkd <cepminkis132@gmail.com>
2021-03-08 02:21:22 +0300

    PIC-22 Добавить возможность загрузки и удаления адреса фоток

--- api/deletePhoto.go
+++ api/deletePhoto.go
@@ -1,12 +1,95 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/gorilla/mux"
 )
 
-func (a *App) DeletePhoto(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r)
-	io.WriteString(w, "delete photo")
+func remove(s []string, i int) []string {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
 }
+
+func (a *App) deletePhoto(userId int, photoId string) (User, error) {
+	var mutex = &sync.Mutex{}
+	mutex.Lock()
+	defer mutex.Unlock()
+	user, ok := a.Users[userId]
+	if !ok {
+		response := errorResponse{Description: map[string]string{}, Err: "Отказано в доступе"}
+		response.Description["id"] = "Пользователя с таким id не найдено"
+		return user, response
+	}
+
+	num := -1
+	for i, v := range user.AvatarAddr {
+		if v == photoId {
+			num = i
+			break
+		}
+
+	}
+	if num == -1 {
+		response := errorResponse{Description: map[string]string{}, Err: "Отказано в доступе"}
+		response.Description["photo_id"] = "Фото с таким id не найдено"
+		return user, response
+	}
+
+	user.AvatarAddr = remove(user.AvatarAddr, num)
+	a.Users[userId] = user
+
+	return user, nil
+}
+
+func (a *App) DeletePhoto(w http.ResponseWriter, r *http.Request) {
+	var photoAddr inputJson
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&photoAddr)
+	if err != nil {
+		responseWithJson(w, 400, err)
+		return
+	}
+
+	token, err := r.Cookie("token")
+	if err != nil {
+		responseWithJson(w, 400, err)
+		return
+	}
+
+	vars := mux.Vars(r)
+	userId, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		responseWithJson(w, 400, err)
+		return
+	}
+
+	if !a.validateCookieForChanging(token.Value, userId) {
+		response := errorResponse{Description: map[string]string{}, Err: "Отказано в доступе, кука устарела"}
+		responseWithJson(w, 401, response)
+		return
+	}
+
+	user, response := a.deletePhoto(userId, photoAddr.addr)
+	if response != nil {
+		responseWithJson(w, 400, response)
+		return
+	}
+
+	user.PasswordHash = nil
+	responseWithJson(w, 200, user)
+
+	fmt.Println("photo deleted")
+}
+
+/*
+curl -b 'token=M3S7xRA8wv0EuDXTk1b0y7OPwQJ5ciqUBi28qiLE' \
+	 --header "Content-Type: application/json" \
+  	 --request PATCH \
+  	 --data '{"mail":"xyz","pass":"xyz","passRepeat":"xyz","oldPass":"xyz1","name":"Kolyan","sex":"female"}' \
+  	 http://localhost:8003/users/0/photos/0
+*/
--- api/uploadPhoto.go
+++ api/uploadPhoto.go
@@ -1,12 +1,79 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/gorilla/mux"
 )
 
-func (a *App) UploadPhoto(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r)
-	io.WriteString(w, "upload photo")
+type inputJson struct {
+	addr string
 }
+
+func (a *App) uploadPhoto(userId int, photoId string) (User, error) {
+	var mutex = &sync.Mutex{}
+	mutex.Lock()
+	defer mutex.Unlock()
+	user, ok := a.Users[userId]
+	if !ok {
+		response := errorResponse{Description: map[string]string{}, Err: "Отказано в доступе"}
+		response.Description["id"] = "Пользователя с таким id не найдено"
+		return user, response
+	}
+
+	user.AvatarAddr = append(user.AvatarAddr, photoId)
+	return user, nil
+}
+
+func (a *App) UploadPhoto(w http.ResponseWriter, r *http.Request) {
+	var photoAddr inputJson
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&photoAddr)
+	if err != nil {
+		responseWithJson(w, 400, err)
+		return
+	}
+
+	token, err := r.Cookie("token")
+	if err != nil {
+		responseWithJson(w, 400, err)
+		return
+	}
+
+	vars := mux.Vars(r)
+
+	userId, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		responseWithJson(w, 400, err)
+		return
+	}
+
+	if !a.validateCookieForChanging(token.Value, userId) {
+		response := errorResponse{Description: map[string]string{}, Err: "Отказано в доступе, кука устарела"}
+		responseWithJson(w, 401, response)
+		return
+	}
+
+	user, response := a.uploadPhoto(userId, photoAddr.addr)
+	if response != nil {
+		responseWithJson(w, 400, response)
+		return
+	}
+
+	user.PasswordHash = nil
+	responseWithJson(w, 200, user)
+
+	fmt.Println("photo uploaded", user)
+}
+
+/*
+	curl -b 'token=oaIXOFJLHaSXzQob5gYPG6b1GOQzzlEE13Oz5bD9' \
+	--header "Content-Type: application/json" \
+  --request POST \
+  --data '{"mail":"xyz","pass":"1234567Qq","passRepeat":"1234567Qq","name":"vasya","birthday":1016048654}' \
+  http://localhost:8003/users/0/photos/dir.png
+*/
