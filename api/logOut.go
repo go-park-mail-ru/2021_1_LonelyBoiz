e1eff2e07acd6829319aa1e832a18e2e5aea3791
nick_nak <nikita.nackaznoy@gmail.com>
2021-03-08 17:18:45 +0300

    PIC-20 Подумать над добавлением редиректов, после logout'а

--- api/logOut.go
+++ api/logOut.go
@@ -2,38 +2,36 @@ package api
 
 import (
 	"net/http"
+	"sync"
 	"time"
 )
 
-func convertStringToRune40(str string) (runes [40]rune) {
-	for i := 0; i < 40; i++ {
-		runes[i] = rune(str[i])
-	}
-	return
-}
+func deleteCookie(key string, sessions *map[int][]http.Cookie) {
+	mutex := &sync.Mutex{}
 
-func deleteCookie(key string, sessions *[]Session) {
-	for i, session := range *sessions {
-		if session.Key == convertStringToRune40(key) {
-			*sessions = append((*sessions)[:i], (*sessions)[i+1:]...)
-			return
+	mutex.Lock()
+	{
+		for id, sessionCookies := range *sessions {
+			for _, sessionCookie := range sessionCookies {
+				if sessionCookie.Name == "token" && sessionCookie.Value == key {
+					delete(*sessions, id)
+				}
+			}
 		}
 	}
+	mutex.Unlock()
 }
 
 func (a *App) LogOut(w http.ResponseWriter, r *http.Request) {
 	cookie, err := r.Cookie("token")
 
 	if err == http.ErrNoCookie || cookie == nil {
-		http.Redirect(w, r, "/", http.StatusFound) //TODO:: куда делать редирект?
 		return
 	}
 
 	key := cookie.Value
+
 	cookie.Expires = time.Now().AddDate(0, 0, -1)
 	http.SetCookie(w, cookie)
-
 	deleteCookie(key, &a.Sessions)
-
-	http.Redirect(w, r, "/", http.StatusFound) //TODO:: куда делать редирект?
 }
