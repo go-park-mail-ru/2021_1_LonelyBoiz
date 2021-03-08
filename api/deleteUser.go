79cdcf422dc0fff48b3c46abaadfd89325929825
nick_nak <nikita.nackaznoy@gmail.com>
2021-03-08 18:00:18 +0300

    PIC-17 Добавить возврат кодов ответа сервера

--- api/deleteUser.go
+++ api/deleteUser.go
@@ -1,12 +1,51 @@
 package api
 
 import (
-	"fmt"
-	"io"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
+func checkAuthorization(a *App, userId int, getKey string) bool {
+	if expectedCookies, ok := a.Sessions[userId]; ok {
+		for _, expectedCookie := range expectedCookies {
+			if expectedCookie.Name == "token" {
+				if expectedCookie.Value != getKey {
+					return true //TODO:: КОД??
+				}
+				break
+			}
+		}
+	}
+	return false
+}
+
 func (a *App) DeleteUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r)
-	io.WriteString(w, "delete user")
+	cookie, cookieError := r.Cookie("token")
+
+	if cookieError == http.ErrNoCookie || cookie == nil {
+		return //TODO:: КОД??
+	}
+
+	getKey := cookie.Value
+
+	args := mux.Vars(r)
+	userId, err := strconv.Atoi(args["id"])
+
+	if err != nil {
+		return //TODO:: КОД??
+	}
+
+	mutex := sync.Mutex{}
+
+	mutex.Lock()
+	{
+		if checkAuthorization(a, userId, getKey) {
+			return
+		}
+
+		delete(a.Users, userId)
+	}
+	mutex.Unlock()
 }
