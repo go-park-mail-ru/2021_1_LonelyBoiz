665fc0b0a1180b07916e7157b5a678db7fc23d7d
Snikimonkd <cepminkis132@gmail.com>
2021-03-10 15:58:59 +0300

    secure

--- api/signUp.go
+++ api/signUp.go
@@ -134,7 +134,7 @@ func (a *App) addNewUser(newUser *User) error {
 func (a *App) setSession(w http.ResponseWriter, id int) {
 	key := KeyGen()
 	expiration := time.Now().Add(24 * time.Hour)
-	cookie := http.Cookie{Name: "token", Value: key, SameSite: 0, Expires: expiration, Domain: "http://localhost:3000", HttpOnly: true, Path: "/"}
+	cookie := http.Cookie{Name: "token", Value: key, SameSite: 0, Expires: expiration, Secure: true, Domain: "http://localhost:3000", HttpOnly: true, Path: "/"}
 
 	http.SetCookie(w, &cookie)
 
