90a29b0a4c58148f498ab1491935a4819d9b96df
Snikimonkd <cepminkis132@gmail.com>
2021-03-11 15:17:49 +0300

    Delete unused handlers declaration

--- api/app.go
+++ api/app.go
@@ -109,8 +109,6 @@ func (a *App) InitializeRoutes(currConfig Config) {
 	a.router.HandleFunc("/users/{id:[0-9]+}", a.ChangeUserInfo).Methods("PATCH")
 	a.router.HandleFunc("/users/{id:[0-9]+}", a.GetUserInfo).Methods("GET")
 	a.router.HandleFunc("/auth", a.GetLogin).Methods("GET")
-	//a.router.HandleFunc("/users/{id:[0-9]+}/photos", a.UploadPhoto).Methods("POST")
-	//a.router.HandleFunc("/users/{id:[0-9]+}/photos", a.DeletePhoto).Methods("DELETE")
 	a.router.HandleFunc("/users/{id:[0-9]+}", a.DeleteUser).Methods("DELETE")
 	a.router.HandleFunc("/login", a.LogOut).Methods("DELETE")
 }
