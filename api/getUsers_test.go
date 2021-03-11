bef1f0f4fb88a826633d6a13a1cd613afdb31b01
Snikimonkd <cepminkis132@gmail.com>
2021-03-11 16:16:12 +0300

    PIC-66 Валидация данных

---
+++ api/getUsers_test.go
@@ -0,0 +1,32 @@
+package api
+
+import "testing"
+
+var TestCaseListUsers = []struct {
+	users  map[int]User
+	finder User
+	res    int
+}{
+	{users: map[int]User{0: {Sex: "male", DatePreference: "male"}}, finder: User{Id: 1, Sex: "male", DatePreference: "male"}, res: 1},
+	{users: map[int]User{0: {Sex: "male", DatePreference: "male"}}, finder: User{Id: 0, Sex: "male", DatePreference: "male"}, res: 0},
+	{users: map[int]User{
+		0: {Sex: "male", DatePreference: "male"},
+		1: {Sex: "male", DatePreference: "male"},
+		2: {Sex: "male", DatePreference: "male"},
+		3: {Sex: "male", DatePreference: "male"},
+		4: {Sex: "male", DatePreference: "male"},
+		5: {Sex: "male", DatePreference: "male"}},
+		finder: User{Id: 6, Sex: "male", DatePreference: "male"},
+		res:    5},
+}
+
+func TestListUsers(t *testing.T) {
+	var a App
+	for _, v := range TestCaseListUsers {
+		a.Users = v.users
+		res := a.listUsers(v.finder)
+		if len(res) != v.res {
+			t.Error("List users error", "\nExpeted:", v.res, "\nGot:", len(res))
+		}
+	}
+}
