a9cfb07de83c9ac0c08e1765048622972fe26eb2
Snikimonkd <cepminkis132@gmail.com>
2021-04-13 01:04:11 +0300

    Убрать запятую перед where

--- proj.session.sql
+++ proj.session.sql
@@ -7,30 +7,18 @@ DROP TABLE users;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    email varchar(51) UNIQUE,
-    name varchar(52),
+    email varchar(50) UNIQUE,
+    name varchar(50),
     passwordHash bytea,
     birthday INT,
+    instagram varchar(50),
     description varchar(250),
-    city varchar(53),
+    city varchar(50),
     sex varchar(10),
     datePreference varchar(10),
     isActive BOOLEAN NOT NULL,
     isDeleted BOOLEAN NOT NULL
 );
-
-CREATE TABLE messages (
-    messageId SERIAL PRIMARY KEY,
-    chatId INT,
-    FOREIGN KEY (chatId) REFERENCES chats (id),
-    authorId INT,
-    FOREIGN KEY (authorId) REFERENCES users (id),
-    text varchar(200) DEFAULT 'empty',
-    time INT,
-    reaction int DEFAULT -1,
-    messageOrder INT
-);
-
 
 CREATE TABLE photos(
     photoId SERIAL PRIMARY KEY,
@@ -44,6 +32,18 @@ CREATE TABLE chats (
     FOREIGN KEY (userId1) REFERENCES users (id),
     userId2 INT,
     FOREIGN KEY (userId2) REFERENCES users (id)
+);
+
+CREATE TABLE messages (
+    messageId SERIAL PRIMARY KEY,
+    chatId INT,
+    FOREIGN KEY (chatId) REFERENCES chats (id),
+    authorId INT,
+    FOREIGN KEY (authorId) REFERENCES users (id),
+    text varchar(200) DEFAULT 'empty',
+    time INT,
+    reaction int DEFAULT -1,
+    messageOrder INT
 );
 
 CREATE TABLE feed (
