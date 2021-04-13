8b582818ddc8986328ff95756abfb9b3b5fd8daa
Snikimonkd <cepminkis132@gmail.com>
2021-04-13 07:31:09 +0300

    Поменять домен куки

--- internal/pkg/session/manager.go
+++ internal/pkg/session/manager.go
@@ -35,7 +35,7 @@ func (session *SessionsManager) SetSession(w http.ResponseWriter, id int) error 
 		Expires:  expiration,
 		SameSite: http.SameSiteNoneMode,
 		Secure:   false,
-		Domain:   "http://localhost:3000",
+		Domain:   "95.163.213.222:8000",
 		HttpOnly: true,
 	}
 
