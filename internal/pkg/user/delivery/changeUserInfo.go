891967c110e874d54a21bcde9717f014f9250093
Snikimonkd <cepminkis132@gmail.com>
2021-04-14 08:03:10 +0300

    добавить логи

--- internal/pkg/user/delivery/changeUserInfo.go
+++ internal/pkg/user/delivery/changeUserInfo.go
@@ -41,6 +41,7 @@ func (a *UserHandler) ChangeUserInfo(w http.ResponseWriter, r *http.Request) {
 		model.ResponseWithJson(w, 400, response)
 		return
 	}
+	a.UserCase.Logger.Info(newUser, "после чтения")
 
 	newUser.Id = userId
 
@@ -66,6 +67,8 @@ func (a *UserHandler) ChangeUserInfo(w http.ResponseWriter, r *http.Request) {
 		model.ResponseWithJson(w, 400, err)
 		return
 	}
+
+	a.UserCase.Logger.Info(newUser, "после обновления")
 
 	newUser.PasswordHash = nil
 	model.ResponseWithJson(w, 200, newUser)
