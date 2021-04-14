3a42b889f042e172011ec880d6f578ed29c14cc7
Snikimonkd <cepminkis132@gmail.com>
2021-04-14 03:27:36 +0300

    ОПменять джомен в куке

--- internal/pkg/session/manager.go
+++ internal/pkg/session/manager.go
@@ -35,7 +35,7 @@ func (session *SessionsManager) SetSession(w http.ResponseWriter, id int) error 
 		Expires:  expiration,
 		SameSite: http.SameSiteNoneMode,
 		Secure:   true,
-		Domain:   "lepick.herokuapp.com",
+		Domain:   "pickle.herokuapp.com",
 		HttpOnly: true,
 	}
 
