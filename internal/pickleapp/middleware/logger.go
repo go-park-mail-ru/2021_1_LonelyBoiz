f8c090d3616e7a6d35a581c436bd3f55490fa059
Snikimonkd <cepminkis132@gmail.com>
2021-04-14 04:59:17 +0300

    ПОменять лог

--- internal/pickleapp/middleware/logger.go
+++ internal/pickleapp/middleware/logger.go
@@ -22,7 +22,7 @@ func (logger *LoggerMiddleware) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
-				logger.Logger.Error(w)
+				logger.Logger.Error(err)
 			}
 		}()
 
