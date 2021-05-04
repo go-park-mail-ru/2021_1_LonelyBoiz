e438bf17ebd7838ad49dbaab290a573127f5fa08
Snikimonkd <cepminkis132@gmail.com>
2021-05-04 18:47:20 +0300

    Пофиксаить прикрепление фотки к чату

--- internal/pkg/models/chat.go
+++ internal/pkg/models/chat.go
@@ -1,17 +1,15 @@
 package models
 
-import (
-	"github.com/google/uuid"
-)
+import "github.com/lib/pq"
 
 type Chat struct {
-	ChatId              int         `json:"chatId" db:"chatid"`
-	PartnerId           int         `json:"partnerId" db:"partnerid"`
-	PartnerName         string      `json:"partnerName" db:"partnername"`
-	LastMessage         string      `json:"lastMessage,omitempty" db:"lastmessage"`
-	LastMessageTime     int64       `json:"lastMessageTime,omitempty" db:"lastmessagetime"`
-	LastMessageAuthorId int         `json:"lastMessageAuthor,omitempty" db:"lastmessageauthorid"`
-	Photos              []uuid.UUID `json:"photos" db:"photos"`
+	ChatId              int            `json:"chatId" db:"chatid"`
+	PartnerId           int            `json:"partnerId" db:"partnerid"`
+	PartnerName         string         `json:"partnerName" db:"partnername"`
+	LastMessage         string         `json:"lastMessage,omitempty" db:"lastmessage"`
+	LastMessageTime     int64          `json:"lastMessageTime,omitempty" db:"lastmessagetime"`
+	LastMessageAuthorId int            `json:"lastMessageAuthor,omitempty" db:"lastmessageauthorid"`
+	Photos              pq.StringArray `json:"photos" db:"photos"`
 }
 
 var ChatsChan = make(chan *Chat)
