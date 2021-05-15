6e596952a9817b6de035a533c486594ceee487c8
Snikimonkd <cepminkis132@gmail.com>
2021-05-15 15:34:49 +0300

    Добавит ьтест на разблокирвоку секретного альбома

---
+++ internal/pkg/user/delivery/unblockSecreteAlbum_test.go
@@ -0,0 +1,121 @@
+package delivery
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	sessionMocks "server/internal/auth_server/delivery/session/mocks"
+	"server/internal/pkg/models"
+	usecaseMocks "server/internal/pkg/user/usecase/mocks"
+	user_proto "server/internal/user_server/delivery/proto"
+	serverMocks "server/internal/user_server/delivery/proto/mocks"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestUnblockSecreteAlbum(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+
+	userUseCaseMock := usecaseMocks.NewMockUserUseCaseInterface(mockCtrl)
+	sessionManagerMock := sessionMocks.NewMockAuthCheckerClient(mockCtrl)
+	serverMock := serverMocks.NewMockUserServiceClient(mockCtrl)
+
+	handlerTest := UserHandler{
+		UserCase: userUseCaseMock,
+		Sessions: sessionManagerMock,
+		Server:   serverMock,
+	}
+
+	user := models.User{
+		Photos: []string{"1", "2"},
+	}
+
+	murl, er := url.Parse("secretAlbum")
+	if er != nil {
+		t.Error(er)
+	}
+
+	req := &http.Request{
+		Method: "GET",
+		URL:    murl,
+	}
+	vars := map[string]string{
+		"id": "1",
+	}
+	req = mux.SetURLVars(req, vars)
+
+	ctx := req.Context()
+	ctx = context.WithValue(ctx,
+		models.CtxUserId,
+		user.Id,
+	)
+
+	rw := httptest.NewRecorder()
+
+	userUseCaseMock.EXPECT().LogInfo(gomock.Any()).Return()
+	serverMock.EXPECT().UnlockSecretAlbum(ctx, &user_proto.UserNothing{}).Return(&user_proto.UserNothing{}, nil)
+	userUseCaseMock.EXPECT().LogInfo(gomock.Any()).Return()
+	userUseCaseMock.EXPECT().LogInfo(gomock.Any()).Return()
+
+	handlerTest.UnblockSecreteAlbum(rw, req.WithContext(ctx))
+
+	response := rw.Result()
+
+	assert.Equal(t, 204, response.StatusCode)
+}
+
+func TestUnblockSecreteAlbum_UnlockSecretAlbum_Error(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+
+	userUseCaseMock := usecaseMocks.NewMockUserUseCaseInterface(mockCtrl)
+	sessionManagerMock := sessionMocks.NewMockAuthCheckerClient(mockCtrl)
+	serverMock := serverMocks.NewMockUserServiceClient(mockCtrl)
+
+	handlerTest := UserHandler{
+		UserCase: userUseCaseMock,
+		Sessions: sessionManagerMock,
+		Server:   serverMock,
+	}
+
+	user := models.User{
+		Photos: []string{"1", "2"},
+	}
+
+	murl, er := url.Parse("secretAlbum")
+	if er != nil {
+		t.Error(er)
+	}
+
+	req := &http.Request{
+		Method: "GET",
+		URL:    murl,
+	}
+	vars := map[string]string{
+		"id": "1",
+	}
+	req = mux.SetURLVars(req, vars)
+
+	ctx := req.Context()
+	ctx = context.WithValue(ctx,
+		models.CtxUserId,
+		user.Id,
+	)
+
+	rw := httptest.NewRecorder()
+
+	userUseCaseMock.EXPECT().LogInfo(gomock.Any()).Return()
+	serverMock.EXPECT().UnlockSecretAlbum(ctx, &user_proto.UserNothing{}).Return(nil, status.Error(codes.Code(500), "Some error"))
+	userUseCaseMock.EXPECT().LogError(gomock.Any()).Return()
+
+	handlerTest.UnblockSecreteAlbum(rw, req.WithContext(ctx))
+
+	response := rw.Result()
+
+	assert.Equal(t, 500, response.StatusCode)
+}
