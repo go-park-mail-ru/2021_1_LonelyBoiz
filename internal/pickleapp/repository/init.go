4519d7bc441452f974dacb634061aafc31871ce5
Snikimonkd <cepminkis132@gmail.com>
2021-05-20 18:23:41 +0300

    Пофиксить подключение к базе ид обавить конфиг нджинкса

--- internal/pickleapp/repository/init.go
+++ internal/pickleapp/repository/init.go
@@ -2,14 +2,13 @@ package repository
 
 import (
 	"database/sql"
+
 	_ "github.com/jackc/pgx"
 	"github.com/jmoiron/sqlx"
 )
 
 func getPostgres() *sql.DB {
-	//dsn := "user=sniki dbname=postgres password=p@ssword1 host=127.0.0.1 port=5432 sslmode=disable"
-	//db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
-	dsn := "dbname=postgres host=localhost port=5432 sslmode=disable"
+	dsn := "user=postgres dbname=postgres host=postgres port=5432 sslmode=disable"
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
 		panic("cant parse config" + err.Error())
